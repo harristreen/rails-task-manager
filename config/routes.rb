ac718c560d1a4bbebabf664477338211ab6fda03
Harris Grant <harris@shoptreen.com>
2023-05-23 17:12:33 +0200

    generated controller, views and model for tasks manager

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # index/list
+  get '/tasks', to: 'tasks#index'
+
+  # create
+  get '/tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # show
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # update
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
